42693389578f3d2466c8ed6be69c654dac18d973
FatumaA <67555014+FatumaA@users.noreply.github.com>
2021-01-14 15:24:42 +0300

    super simple html only validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+// import {useForm} from 'react-hook-form'
 import {ButtonInfo} from './Data'
 import {ContactInfo} from './Data'
 
 const Contact = () => {
 
-  const {register, errors, handleSubmit}=useForm({
-    mode: 'onSubmit',
-})
-
-const[state, setState] = React.useState({})
-const handleChange = (e) => setState ({ ...state, [e.target.name]: e.target.value })
-const onSubmit =(data, e) =>{
-  e.preventDefault()
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encodeURI({
-      'form-name': 'contact',
-      ...state
-    })
-  })
-    .then(()=> '/contact')
-    .catch(error => alert(error)); 
-}
+//   const {register, errors, handleSubmit}=useForm({
+//     mode: 'onSubmit',
+// })
+// const handleChange = (e) => setState ({ ...state, [e.target.name]: e.target.value })
+// const onSubmit =(data, e) =>{
+//   e.preventDefault()
+//   fetch("/", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: encodeURI({
+//       'form-name': 'contact',
+//       ...state
+//     })
+//   })
+//     .then(()=> '/contact')
+//     .catch(error => alert(error)); 
+// }
 
   return (
     <div id='contact'>
@@ -41,49 +39,50 @@ const onSubmit =(data, e) =>{
 
         <form name='contact' 
         action='/contact' method='POST' 
-        onSubmit={handleSubmit(onSubmit)}>
+        // onSubmit={handleSubmit(onSubmit)}
+        >
         <input type='hidden' name='form-name' value='contact'/>
         <label htmlFor='Name'>Name:</label>
         <input 
                   type='text' 
                   name='name'
-                  onChange={handleChange}
-                  placeholder ='Name...'
-                  ref={register({ required: true })}
+                  // onChange={handleChange}
+                  // placeholder ='Name...'
+                  // ref={register({ required: true })}
                />
-               <div>
+               {/* <div>
               {errors.name && <span style={{color:'red'}}> Please input your name</span>}
-              </div>
+              </div> */}
                <br/>
         <label htmlFor='Email'>Email:</label>
         <input 
                    type='email' 
                    name='email' 
-                   onChange={handleChange}
+                  //  onChange={handleChange}
                    placeholder='Email...' 
-                   ref={register({
-                   required: 'Required',
-                   pattern:{
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                        message: 'Please use a valid email address'
-                            }
-                    })}
+                  //  ref={register({
+                  //  required: 'Required',
+                  //  pattern:{
+                  //       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  //       message: 'Please use a valid email address'
+                  //           }
+                  //   })}
                />
-               <div>
+               {/* <div>
                {errors.email && <span style={{color:'red'}}>{errors.email.message}</span>}
-               </div>
+               </div> */}
                <br/>
       <label htmlFor='Message'>Message:</label>
       <textarea  rows='20' name='message' 
-      onChange={handleChange} 
-      ref={register({
-                   required: true
-               })}
+      // onChange={handleChange} 
+      // ref={register({
+      //              required: true
+      //          })}
                placeholder='Type your message here...'
                />
-               <div>
+               {/* <div>
                {errors.message && <span style={{color:'red'}}>Please add a message</span>}
-               </div>
+               </div> */}
                <br/>
                
                <button type='submit' className='btn-primary'>
